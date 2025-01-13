434df609c31058da4b9826aba8eb96319dbac296
Jesús Enrique Bonilla España <80067360+eren090011@users.noreply.github.com>
2025-01-13 15:27:10 -0500

    qwwqwq

--- BlazorPeliculas/Client/obj/Debug/net7.0/BlazorPeliculas.Client.AssemblyInfo.cs
+++ BlazorPeliculas/Client/obj/Debug/net7.0/BlazorPeliculas.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbdbf4096316a44d855de4a458088ed453883043")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16fd2cf0dbfeee011dbcf54c67c0ec5e5b1d353f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
